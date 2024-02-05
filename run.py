261eb17da4bb2c327741e46c1a6466c7d6e2d05e
sanchez619 <>
2024-02-05 19:23:06 +0000

    feat: set global character constants

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Dream Mansion - a text based game
+"""
+# Global Variables - especially inventories and stats
+
+FOCUS = 5
+CONSTITUTION = 5
+LUCK = 5
+
