f7c996a9be1fdc66079ea33b698066414d1bbe35
sanchez619 <>
2024-02-05 19:50:48 +0000

    feat: add room interface menu print message

--- run.py
+++ run.py
@@ -18,3 +18,22 @@ print("I'm so tired...")
 print("...")
 print("...")
 initiate_game = input("Do you want to wake up? (Yes = 1; No = 0)")
+if initiate_game == "0":
+    print("...")
+    print("...")
+    print("You close your eyes, one last time...")
+elif initiate_game == "1":
+    print("...")
+    print("...")
+    print("You wake up in an unknown location...")
+    print("...")
+    print("...")
+
+
+# Menu for interacting in rooms
+def room_menu():
+    print("You are in the 'filler text'")
+    print("1. Investigate")
+    print("2. Perceive")
+    print("3. Rearrange")
+    print("0: Change rooms")
