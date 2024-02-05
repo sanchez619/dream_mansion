1ab7cf70b99828cb01119a5fa80d41cef73f7882
sanchez619 <>
2024-02-05 20:14:05 +0000

    fix: adjust function name

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ CONSTITUTION = 5
 LUCK = 5
 
 # Status of charater, determines used interface
-
+GAME_STATUS = False
 CURRENT_STATUS = "inRoom"
 
 # Inventory List
@@ -32,6 +32,7 @@ elif initiate_game == "1":
     print("You wake up in an unknown location...")
     print("...")
     print("...")
+    True
 
 
 # Menu for interacting in rooms
@@ -42,15 +43,35 @@ def room_menu():
     print("3. Rearrange")
     print("0: Change rooms")
 
+
 def investigate():
     print("You carefully investigate the room...")
 
+
 def perceive():
-    print("You perceive your surroundings, trying to gather more information...")
+    print("You perceive your surroundings, trying to gather information...")
+
 
 def rearrange():
     print("You rearrange items in the room, looking for hidden clues...")
 
+
 def change_rooms():
     print("You head to another room...")
     print("You enter...")
+
+
+while True:
+    if CURRENT_STATUS == "inRoom":
+        room_menu()
+        choice = input("Enter your choice (0-3): ")
+        if choice == '0':
+            change_rooms()
+        elif choice == '1':
+            investigate()
+        elif choice == '2':
+            perceive()
+        elif choice == '3':
+            rearrange()
+        else:
+            print("Invalid choice. Please enter a number between 1 and 3.")
