9854208f4a1ed982a0e4fea7788fa920b1a128c6
Sebastian Bucher <Sebasti.b@web.de>
2024-02-13 15:13:15 +0000

    feat: add basic class structure

---
+++ library.py
@@ -0,0 +1,13 @@
+class Events:
+    def __init__(self, name, trigger, effect):
+        self.name = name
+        self.trigger = trigger
+        self.effect = effect
+
+
+
+class Items:
+
+
+
+class Endings:
